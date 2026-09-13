4f6b5fb21db58676f2e98503caef28ee4a6bb352
koddharck <davidayomide2018@gmail.com>
2026-09-13 17:50:06 +0100

    refactor: streamline Calculator component by integrating history handling in AppContent

--- Adv-Calculator/src/components/Calculator/Calculator.jsx
+++ Adv-Calculator/src/components/Calculator/Calculator.jsx
@@ -1,25 +1,8 @@
 import styles from "./Calculator.module.css";
 import Display from "../Display/Display";
 import Keypad from "../Keypad/Keypad";
-import useCalculator from "../../hooks/useCalculator";
-import { useHistoryContext } from "../../context/HistoryContext";
 
-export default function Calculator() {
-  const calculator = useCalculator();
-  const { addEntry } = useHistoryContext();
-
-  function handleEqualsAndSave() {
-    const completed = calculator.handleEquals();
-    if (completed) {
-      addEntry(completed.expression, completed.result);
-    }
-  }
-
-  const calculatorWithHistory = {
-    ...calculator,
-    handleEquals: handleEqualsAndSave,
-  };
-
+export default function Calculator({ calculator }) {
   return (
     <div className={styles.calculator}>
       <Display
@@ -27,7 +10,7 @@ export default function Calculator() {
         operator={calculator.operator}
         previousValue={calculator.previousValue}
       />
-      <Keypad calculator={calculatorWithHistory} />
+      <Keypad calculator={calculator} />
     </div>
   );
 }
